2f47cb5da52a152368116be00d006231ad7d868e
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-24 17:09:55 +0530

    Create global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {GlobalStyles} from './GlobalStyles';
 
 function App() {
   return (
     <div className="App">
       Start here.
+      <GlobalStyles />
     </div>
   );
 }
---
+++ src/GlobalStyles.js
@@ -0,0 +1,42 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle `
+    :root{
+        --maxWidth : 1280px;
+        --white : #FFF;
+        --lightGrey : #EEE;
+        --medGrey : #353535;
+        --darkGrey : #1c1c1c;
+        --fontLarger : 2.5rem;
+        --fontLarge : 1.5rem;
+        --fontMed : 1.2rem;
+        --fontSmall : 1rem;
+    }
+
+    *{
+        box-sizing : border-box;
+        font-family : 'Abel' , sans-serif;
+    }
+
+    body{
+        margin : 0;
+        padding : 0;
+
+        h1{
+            font-size : 2rem;
+            font-waight : 600;
+            color : var(--white);
+        }
+
+        h3{
+            font-size : 1.1rem;
+            font-waight : 600;
+        }
+
+        p{
+            font-size : 1rem;
+            color : var(--white);
+        }
+    }
+
+`
