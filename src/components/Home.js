8b9f62bc04f957805c4dd7761d48b2da9697bcdb
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-24 20:21:59 +0530

    Edit Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
-import React , {useState} from 'react';
+import React , {useState,useEffect} from 'react';
+import API from '../API';
 //config
 import {
     POSTER_SIZE,
@@ -14,6 +15,29 @@ const Home = () => {
     const[state,setState] = useState();
     const[loading,setLoading] = useState(false);
     const[error,setError] = useState(false);
+
+    const fetchMovies = async (page,searchTerm="") => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm,page);
+            console.log(movies);
+
+            setState(prev =>({
+                ...movies,
+                results: page > 1 ? [...prev.results , ...movies.results] : [...movies.results]
+            }))
+
+        }catch(err){
+            setError(true);
+        }
+
+        setLoading(false);
+    }
+
+    //Initial render
+    useEffect(()=>fetchMovies(1),[]);
 
     return (
         <div>
