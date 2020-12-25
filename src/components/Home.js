4ada81d41ad5dac1c1108353712a762719f53712
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-25 21:26:46 +0530

    Implement HeroImage component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React,{Fragment}from 'react';
 //config
 import {
     POSTER_SIZE,
     BACKDROP_SIZE,
     IMAGE_BASE_URL
 }from '../config';
-
 //images
 import NoImage from '../images/no_image.jpg';
-
 //hooks
 import {useMovieFetch} from '../hooks/useHomeFetch';
+//components
+import HeroImage from './HeroImage/HeroImage';
 
 const Home = () => {
 
     const {state,loading,error} = useMovieFetch();
 
     return (
-        <div>
-            home page
-        </div>
+        <Fragment>
+
+            {state.results[0] ? 
+            <HeroImage 
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0]}.backdrop_path`}
+                title={state.results[0].original_title}
+                text={state.results[0].overview}
+            /> 
+            : null}
+            
+        </Fragment>
     )
 }
 
