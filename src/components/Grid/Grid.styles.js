fde0674dddd1b4fa27dbbc2d9d035c5d6c88bc7f
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-26 23:21:35 +0530

    Implement Grid component

--- src/components/Grid/Grid.styles.js
+++ src/components/Grid/Grid.styles.js
@@ -1,5 +1,25 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div ``
+export const Wrapper = styled.div `
 
-export const Content = styled.div`` 
+    max-width : var(--maxWidth);
+    margin : 0 auto;
+    padding : 0 20px;
+
+    h1{
+        color : var(--medGrey);
+
+        @media screen and (max-width : 768px) {
+            font-size : var(--fontLarge);
+        }
+    }
+
+`
+
+export const Content = styled.div`
+
+    display : grid;
+    grid-template-column : repeat(auto-fill,minmax(200px,1fr));
+    grid-gap : 2rem;
+
+` 
